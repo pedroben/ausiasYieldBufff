ad64a74cc8039a74264b7386d47cfe166367f1d9
Rafael Aznar <rafaaznar@gmail.com>
2013-11-11 22:38:36 +0100

    getCount

--- src/main/java/net/daw/data/GenericData.java
+++ src/main/java/net/daw/data/GenericData.java
@@ -1,4 +1,3 @@
-
 package net.daw.data;
 
 import java.util.ArrayList;
@@ -37,6 +36,8 @@ public interface GenericData {
     public Boolean existsOne(String strTabla, int id) throws Exception;
 
     public int getPages(String strTabla, int intRegsPerPage,ArrayList<FilterBean> alFilter, HashMap<String, String> hmOrder) throws Exception;
+    
+    public int getCount(String strTabla, ArrayList<FilterBean> alFilter) throws Exception;
 
     public ArrayList<Integer> getPage(String strTabla, int intRegsPerPage, int intPagina, ArrayList<FilterBean> alFilter, HashMap<String, String> hmOrder) throws Exception;
 
